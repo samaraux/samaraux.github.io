3487bb471fcf4468985393fa9e05b2913084f798
samaraux <80707820+samaraux@users.noreply.github.com>
2021-05-14 08:28:08 -0300

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,8 @@
+const nome = prompt('digite seu nome')
+const anoNascimento = prompt ('digite seu ano de nascimento')
+
+let anoAtual = 2021;
+
+idade = anoAtual - anoNascimento ;
+
+alert("Olá, seu nome é " + nome + " e você tem " + idade +  "anos");
